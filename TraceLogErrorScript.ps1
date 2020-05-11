191fcae97e37341d5c3cd85bfec402812e21fb0b
viplov8912 <48627660+viplov8912@users.noreply.github.com>
2020-05-11 13:26:35 +0530

    Add files via upload

---
+++ TraceLogErrorScript.ps1
@@ -0,0 +1,20 @@
+﻿
+$logfile = (Get-Content C:\Users\viplov\Documents\testzabbix-master\alert_flexnet.log)
+$values = $logfile -match '.*ORA-.*$'
+
+
+
+foreach($line in $values) {
+    if($line -match '.*ORA-48913.*$' -or $line -match '.*ORA-609.*$' -or $line -match '.*ORA-279.*$'){
+       #Write-Output('can be ignored')
+       continue
+    }else
+    
+    {
+    
+       Write-Host($line) >> C:\Users\viplov\Documents\testzabbix-master\output_log.txt
+      
+    }
+}
+
+Write-Host('done!!')
